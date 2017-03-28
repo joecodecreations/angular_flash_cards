f0855588c96e7d715dee6b18aeaa8e9fc63abc9e
Joseph <joseph@codecreations.net>
2017-03-28 14:23:57 -0400

    updated string for url

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(helmet({
 /////////////////////////////////////////
 //           Mongo Database            //
 ////////////////////////////////////////
-mongoose.connect('mongodb://' + resources.USERNAME + ':' + resources.PASSWORD + '@localhost:' + resources.PORT + '/flashcardquiz');
+mongoose.connect('mongodb://' + resources.USERNAME + ':' + resources.PASSWORD + '@localhost:' + resources.PORT + '/admin');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:' + resources.PORT));
 /////////////////////////////////////////
