06b64a183c2d3722e43dbaad4517789c0b3aefc6
Joseph <joseph@codecreations.net>
2017-03-28 13:24:04 -0400

    adding updated db connect

--- connect.js
+++ connect.js
@@ -1,8 +1,12 @@
+/* secured resources not shared on github */
+require('/private/resources.js');
+
+
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
 // Connection URL
-var url = 'mongodb://localhost:27017/myproject';
+var url = 'mongodb://' + mongoUSERNAME + ':' + mongoPASSWORD + 'localhost:' + mongoPORT + '/myproject';
 // Use connect method to connect to the Server
 MongoClient.connect(url, function (err, db) {
     assert.equal(null, err);
