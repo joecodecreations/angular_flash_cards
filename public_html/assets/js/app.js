f869fb06c1b6397575af173946f9b4361b1d4d04
Joseph <joseph@codecreations.net>
2017-03-31 18:57:34 -0400

    routes updated

--- public_html/assets/js/app.js
+++ public_html/assets/js/app.js
@@ -457,16 +457,48 @@ function resetValidationService() {
 },{}],8:[function(require,module,exports){
 angular.module('app').service('retrieve', retrieveService);
 
-function retrieveService($rootScope, $location) {
+function retrieveService($rootScope, $location, $http) {
 
     var vm = this;
 
     /* Add a new card */
     vm.deck = function ($scope) {
-        // var url = $location.absUrl().split('/')[0];
+
+        //grab url in browser bar
         var url = $location.absUrl();
+        //strip out url and get only oken
         var token = url.replace("https://flashcardquiz.com/", "");
-        console.log("window Location:" + token);
+
+        //if we have a token let's grab the data if any
+        if (token) {
+
+            //hit the API
+            $http({
+                method: 'GET',
+                url: '/api/gather/' + token,
+                //data: deckInfo,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(function successCallback(response) {
+                if (response.data.message == "success") {
+
+                    ctrl.questions = response.data.cards;
+                    ctrl.deckTitle = response.data.title;
+                    ctrl.backgroundColor = response.data.backgroundColor;
+                    ctrl.canSkipQuestions = response.data.canSkipQuestions;
+                    ctrl.route = token;
+
+                    console.log(ctrl.questions);
+
+                }
+
+            });
+
+        } else {
+            console.log("We had no token to process");
+        }
+
 
     };
 
