1b43e0f6de8ab42d3e96ece8c4059edead1802d6
Joseph <joseph@codecreations.net>
2017-03-31 16:41:27 -0400

    adding in route test url express

--- server.js
+++ server.js
@@ -85,8 +85,7 @@ app.get('/api/decks/get/all', function (req, res) {
 
 app.get('/*', function (req, res) {
     var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    console.log(fullUrl);
-    res.send("worked!");
+    res.render('.' + path + '/pages/index', {});
 });
 
 module.exports = app; //Export instead of creating server
